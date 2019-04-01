3c5bc0d0cd64706b25811f0f2c9d7a77de81656c
Nicolas Bazille <nicolas.bazille@m6.fr>
2019-04-01 13:03:12 +0100

    Parser: check Javascript syntax before

--- src/parser.js
+++ src/parser.js
@@ -1,20 +1,69 @@
 "use strict";
 
 const { execSync } = require("child_process");
+const _ = require("lodash");
+const {
+  __debug: { parse }
+} = require("prettier");
 
 const escapeString = str => str.replace(/("|`)/g, "\\$1");
 
-const parse = text => {
+function createError(message, loc) {
+  // Construct an error similar to the ones thrown by Babel.
+  const error = new SyntaxError(
+    message + " (" + loc.start.line + ":" + loc.start.column + ")"
+  );
+  error.loc = loc;
+  return error;
+}
+
+function checkJavascriptValue(node) {
+  try {
+    parse(node.value, { parser: "babel" });
+  } catch (err) {
+    if (err instanceof SyntaxError) {
+      const { loc } = err;
+
+      if (loc.start.line === 1) {
+        loc.start.column += node.loc.start.column;
+      }
+
+      loc.start.line += node.loc.start.line - 1;
+
+      const [message] = err.message.split(/ \(\d+:\d+\)/);
+
+      throw createError(message, loc);
+    } else {
+      throw err;
+    }
+  }
+}
+
+function checkJavascriptNodes(node) {
+  if (node.kind === "JavascriptValue" || node.kind === "JavascriptBlock") {
+    checkJavascriptValue(node);
+  }
+
+  if (_.get(node, ["value", "kind"])) {
+    checkJavascriptNodes(node.value);
+  }
+
+  if (node.children && node.children.length > 0) {
+    node.children.forEach(checkJavascriptNodes);
+  }
+}
+
+module.exports = text => {
   const bin = require.resolve("@oltodo/qml-parser");
 
-  let result = execSync(`${bin} "${escapeString(text)}"`);
-  result = JSON.parse(result);
+  const result = execSync(`${bin} "${escapeString(text)}"`);
+  const ast = JSON.parse(result);
 
-  if (result === null) {
+  if (ast === null) {
     return {};
   }
 
-  return result;
-};
+  checkJavascriptNodes(ast);
 
-module.exports = parse;
+  return ast;
+};
